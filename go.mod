d6e132d7137b69a78636a5c295a40c8918dbd74a
Swapnil Daddikar <swapnildaddikar07@gmail.com>
2022-03-21 13:31:25 +0530

    First commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SwapnilDaddikar07/library-test
+
+go 1.17
---
+++ string-utils/string-utils.go
@@ -0,0 +1,7 @@
+package string_utils
+
+import "fmt"
+
+func PrintLength(s string) {
+	fmt.Println("Length is ", len(s))
+}
